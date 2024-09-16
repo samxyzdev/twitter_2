e5d8383de6a5204d2613de8229fb77577d58b9ab
Sameer Ahmed <sameer937655@gmail.com>
2024-09-16 15:56:40 +0530

    add sidebar and tweet button

--- twitter-client/app/page.tsx
+++ twitter-client/app/page.tsx
@@ -1,13 +1,66 @@
 import Image from "next/image";
-import { BsTwitter } from "react-icons/bs";
+import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
+import React from "react";
+import { BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+interface TwitterSidebarButton {
+  title: string;
+  icon: React.ReactNode;
+}
+
+const sidebarMenuItems: TwitterSidebarButton[] = [
+  {
+    title: "Home",
+    icon: <BiHomeCircle />,
+  },
+  {
+    title: "Explore",
+    icon: <BiHash />,
+  },
+  {
+    title: "Notifications",
+    icon: <BsBell />,
+  },
+  {
+    title: "Messages",
+    icon: <BsEnvelope />,
+  },
+  {
+    title: "Bookmarks",
+    icon: <BsBookmark />,
+  },
+  {
+    title: "Profile",
+    icon: <BiUser />,
+  },
+];
 
 export default function Home() {
   return (
-    <div>
+    <div className={inter.className}>
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className="col-span-3 flex justify-start pt-8">
-          <div className="text-4xl h-fit hover:bg-gray-600 rounded-full p-2 cursor-pointer transition-all">
+        <div className="col-span-3 pt-8 px-4">
+          <div className="text-4xl h-fit w-fit hover:bg-gray-600 rounded-full p-4 cursor-pointer transition-all">
             <BsTwitter />
+          </div>
+          <div className="mt-4 text-2xl font-semibold pr-4">
+            <ul>
+              {sidebarMenuItems.map((item) => (
+                <li
+                  className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-5 py-2 w-fit cursor-pointer mt-2"
+                  key={item.title}
+                >
+                  <span>{item.icon}</span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <button className="bg-[#1d9bf0] mt-5 p-4 rounded-full w-full">
+              Tweet
+            </button>
           </div>
         </div>
         <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-400"></div>
